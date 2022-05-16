370c0b6c69994287005e0d5437a4968d480d9675
Jose Rios <roraj77@hotmail.com>
2022-05-15 21:33:59 -0500

    Agregando el algoritmo 07 version final

--- Algoritmos/07multiplicaciomMatrices.py
+++ Algoritmos/07multiplicaciomMatrices.py
@@ -1,50 +1,51 @@
-
 import threading
-from threading import Thread
 import math
-import os
-
-A = [[0 for _ in range(2)] for _ in range(2)]
-B = [[0 for _ in range(2)] for _ in range(2)]
-C = [[[0 for _ in range(2)] for _ in range(2)] for _ in range(2)]
-C2 = [[[0 for _ in range(2)] for _ in range(2)] for _ in range(2)]
-
-def hilo1(i, j, k):
-    C[k][i][j] = int(A[i][k]) * int(B[k][j])
 
 
-def hilo2(i, j, k, l):
-    if (((2 * k) % (2 ** l)) == 0):
-        C2[2 * k][i][j] = int(C[2 * k][i][j] + C[2 * k - (2 ** (l))][i][j])
+
+A=[[0 for _ in range(2)] for _ in range(2)]
+B=[[0 for _ in range(2)] for _ in range(2)]
+C=[[[0 for _ in range(2)] for _ in range(2)] for _ in range(2)]
+C2=[[[0 for _ in range(2)] for _ in range(2)] for _ in range(2)]
+
+
+def hilo1(i,j,k):
+    C[k][i][j]=int(A[i][k])* int(B[k][j])
+
+def hilo2(i,j,k,l):
+    if(((2*k) % (2 **l))==0):
+        C2[2*k][i][j]=int(C[2*k][i][j]+C[2*k-(2**(l))][i][j])
+
+def llenarMatrices(A, B):
+    print("Digite los datos de la matriz A")
+    i = 0
+    while (i < 2):
+        j = 0
+        while (j < 2):
+            print("Digite el valor [", i + 1, ", ", j + 1, " ]: ")
+            dato = int(input())
+            A[i][j] = dato
+            j += 1
+        i += 1
+
+    print("Digite los datos de la matriz B")
+    i = 0
+    while (i < 2):
+        j = 0
+        while (j < 2):
+            print("Digite el valor [", i + 1, ", ", j + 1, " ]: ")
+            dato = int(input())
+            B[i][j] = dato
+            j += 1
+        i += 1
+
 
 def main():
     lg = int(math.log(2, 2))
-    print ("\n\n             LLENADO DE LA MATRIZ A:" )
-    i = 0
-    while (i < 2):
-        j = 0
-        while (j < 2):
-            print ("INGRESE EL VALOR DE LA COORDENADA [", i + 1, ", ", j + 1, " ]: " )
-            x = int(input())
-            A[i][j] = x
-        j = j + 1
-    i = i + 1
-
-    print ("\n\n             LLENADO DE LA MATRIZ B:" )
-    i = 0
-    while (i < 2):
-        j = 0
-        while (j < 2):
-            print ("INGRESE EL VALOR DE LA COORDENADA [", i + 1, ", ", j + 1, " ]: " )
-            x = int(input())
-            B[i][j] = x
-            j = j + 1
-        i = i + 1
-
-    print ( "\nPROCEDIMIENTO DE LAS MULTIPLICACIONES : \n" )
-    print ("[ ", A[0][0], "  ", A[0][1], " ]      X      [ ", B[0][0], "  ", B[0][1], " ]" )
+    llenarMatrices(A,B)
+    print ("\nPROCEDIMIENTO DE LAS MULTIPLICACIONES : \n" )
+    print ("[ ", A[0][0], "  ", A[0][1], " ]      X      [ ", B[0][0], "  ", B[0][1], " ]")
     print ("[ ", A[1][0], "  ", A[1][1], " ]      X      [ ", B[1][0], "  ", B[1][1], " ]")
-
     k = 0
     while (k < 2):
         i = 0
@@ -54,12 +55,11 @@ def main():
                 t = threading.Thread(target=hilo1, args=(i, j, k))
                 t.start()
                 t.join()
-                j = j + 1
-            i = i + 1
-        k = k + 1
+                j+=1
+            i +=1
+        k += 1
 
-    print ("\n\nPRIMER PROCESO: ", C)
-
+    print ("\n\nPrimer proceso: ", C)
     l = 0
     while (l < lg):
         i = 0
@@ -68,19 +68,19 @@ def main():
             while (j < 2):
                 k = 0
                 while (k < 1):
-                    t = Thread(target=hilo2, args=(i, j, k, l))
+                    t = threading.Thread(target=hilo2, args=(i, j, k, l))
                     t.start()
                     t.join()
                     k = k + 1
-                j = j + 1
-            i = i + 1
-        l = l + 1
-
-    print ("\nRESULTADO de LAS MULTIPLICACIONES DE LAS MATRICES A Y B: \n")
-
-    print ("          [ ", C2[0][0][0], "  ", C2[0][0][1], " ]")
-    print ("          [ ", C2[0][1][0], "  ", C2[0][1][1], " ]" )
+                j +=  1
+            i += 1
+        l +=  1
+    print ( "\nRESULTADO de A X B: \n" )
+    print ("          [ ", C2[0][0][0], "  ", C2[0][0][1], " ]" )
+    print ("          [ ", C2[0][1][0], "  ", C2[0][1][1], " ]")
 
 
 if __name__ == '__main__':
     main()
+
+
