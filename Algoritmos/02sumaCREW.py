431b750b8ed46d4ad8511097f8782be2053136f0
Jose Rios <roraj77@hotmail.com>
2022-05-18 16:59:13 -0500

    02 Listo

--- Algoritmos/02sumaCREW.py
+++ Algoritmos/02sumaCREW.py
@@ -7,24 +7,25 @@ import threading
 import math
 
 def  thread01(i,j,A,B):
-    B[j]=A[j]+A[(j-2**((i)-1))]
+    B[j] = A[j] + A[(j-2**((i)-1))]
 
 def sumaEREW():
     A = [0, 5, 2, 10, 1, 8, 12, 7, 3]
     B = A.copy()
     n = len(A) - 1
     lg = int(math.log(n, 2))
+    print("-" * 37)
     print('Programa 2. SUMA CREW')
-    print('-' * 33)
+    print("-" * 37)
     print(A)
-    print('-' * 33)
+    print('-' * 37)
     for i in range(1, lg + 1):
         for j in range((2 ** (i - 1) + 1), n + 1):
             h = threading.Thread(target=thread01, args=(i, j, A, B))
             h.start()
             h.join()
         A = B.copy()
-        print(A)
+        print("-->",A)
 
 def main():
     sumaEREW()
