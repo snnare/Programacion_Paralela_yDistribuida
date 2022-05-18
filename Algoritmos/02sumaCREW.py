a5616e758db3646f210fcf1e920e122bc640d305
Jose Rios <roraj77@hotmail.com>
2022-05-17 22:21:15 -0500

    02 modo chido

--- Algoritmos/02sumaCREW.py
+++ Algoritmos/02sumaCREW.py
@@ -1,25 +1,41 @@
+''' Programa 02 SUMA CREW
+Profesor: Elfego Gutierrez Ocampo
+Alumno: Jose Angel Romero Rios
+'''
+
+import threading
 import math
-import  threading
 
-def thread (i, j, A):
-    A[j] = A[j] + A [j-pow(2,i-1)]
+def  thread01(i,j,A,B):                                 # Definiendo el hilo (thread)
+    B[j]=A[j]+A[(j-2**((i)-1))]
+
+
+def sumaEREW():
+    A = [0, 5, 2, 10, 1, 8, 12, 7, 3]
+    B = A.copy()
+    n = len(A) - 1
+    lg = int(math.log(n, 2))
+    threads = []
+    print('Programa 2. SUMA CREW')
+    print('-' * 33)
     print(A)
+    print('-' * 33)
+    for i in range(1, lg + 1):
+        for j in range((2 ** (i - 1) + 1), n + 1):
+            h = threading.Thread(target=thread01, args=(i, j, A, B))
+            #threads.append(h)
+            h.start()
+            h.join()
 
-def sumaCREW():
-    A = [0,5,2,10,1,8,12,7,3]
-    n = len(A)
-    lg = int(math.log(n,2))
-    for i in range( 1, lg-1):
-        for j in range((pow(2,i-1)+1),n):
-            t = threading.Thread(target=thread(i,j,A))
-            t.start()
-            t.join()
-            print(A)
-    print(A)
-
+        #for _ in threads:
+           # h.join()
+        A = B.copy()
+        print(A)
 
 def main():
-    sumaCREW()
+    sumaEREW()
+
+
 
 if __name__ == '__main__':
     main()
