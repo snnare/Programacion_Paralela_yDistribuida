434a7d90f72dc745004f06cca0e5f3cb5cb93084
Jose Rios <roraj77@hotmail.com>
2022-05-18 19:47:11 -0500

    04 Listo

--- Algoritmos/04busquedaCRCW.py
+++ Algoritmos/04busquedaCRCW.py
@@ -1,37 +1,48 @@
+''' Programa 04 BUSQUEDA CRCW
+Profesor: Elfego Gutierrez Ocampo
+Alumno: Jose Angel Romero Rios
+'''
+
 import threading
 
 def MinCRCW (lista, win, n):
-    print("\n \t Busqueda CRCW ")
+    print('-' * 63)
+    print('\t' * 4, 'Busqueda CRCW')
+    print('-' * 63)
     for i in range(n):
         win[i] = 0
+    print('-' * 63)
     print("Primer paso 1: \n",win)
-
+    print('-' * 63)
     for i in range(n):
         for j in range(n):
             if lista[i] > lista[j]:
                 win[i] = 1
+    print('-' * 63)
     print("Segundo paso 2: \n",win)
-
+    print('-' * 63)
     for i in range(n):
         if(win[i] == 0):
             indexMin = i
-    print("Tercer paso 3: \n indexmin:", indexMin)
-
-    print("\n El minimo es: : ",lista[indexMin])
-
-
+    print('-' * 63)
+    print("El elemento minimo es: ",lista[indexMin]," con IndexMin:", indexMin)
+    print('-' * 63)
+    print('-' * 63)
 
 def main():
-    lista = []
-    win = []
+    array = [95,10,6,15]
+    win = [9 for _ in range (len(array))]
+    '''  En caso de querer digitar los datos del arreglo manualmente
     n = int(input("Ingrese el tamanio del arreglo: "))
     for i in range(n):
         dato = int(input("Digite un numero: "))
-        lista.append(dato)
+        array.append(dato)
         win.append(9)
-        print(lista)
-    print(lista)
-    t = threading.Thread(target=MinCRCW(lista,win,n))
+        print(array)
+    print(array)
+    '''
+    #win.append(9)
+    t = threading.Thread(target=MinCRCW(array,win,len(array)))
     t.start()
     t.join()
 
